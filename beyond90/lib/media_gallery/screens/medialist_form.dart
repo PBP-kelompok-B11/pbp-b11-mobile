d377bebe55a5373f79b54f59ae78aee05f51eadf
NisriinaWakhdah <ninaputri3344@gmail.com>
2025-12-01 16:07:01 +0700

    Add models, screens, and widgets folders for each moduls

--- beyond90/lib/media_gallery/screens/medialist_form.dart
+++ beyond90/lib/media_gallery/screens/medialist_form.dart
@@ -1,1 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:beyond90/app_colors.dart';
+
+class MediaFormPage extends StatefulWidget{
+  const MediaFormPage({super.key});
+
+  @override
+  State<MediaFormPage> createState() => _MediaFormPageState();
+
+}
+
+class _MediaFormPageState extends State<MediaFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _deskripsi = "";
+  String _kategori = "foto"; // default
+  String _thumbnail = "";
+
+
+  final List<String> _categories = [
+    'foto',
+    'video'
+  ];
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Upload Media'),
+        ),
+        backgroundColor: AppColors.indigo,
+        foregroundColor: AppColors.white,
+      ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              // Deskripsi
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Masukkan deskripsi media",
+                    labelText: "Deskripsi",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (String? value){
+                    setState(() {
+                      _deskripsi = value!;
+                    });
+                  },
+                  validator: (String? value){
+                    if (value == null || value.isEmpty){
+                      return "Please fill in this field";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
