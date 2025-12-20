0347d2cf4d39bbd1ed647a08297cfa13f962bc5a
NisriinaWakhdah <ninaputri3344@gmail.com>
2025-12-21 01:17:36 +0700

    fixing login and logout flutter

--- beyond90/lib/media_gallery/widgets/media_entry_card.dart
+++ beyond90/lib/media_gallery/widgets/media_entry_card.dart
@@ -1,6 +1,7 @@
 import 'package:beyond90/media_gallery/models/media_entry.dart';
 import 'package:flutter/material.dart';
 import 'package:beyond90/app_colors.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class MediaEntryCard extends StatelessWidget {
   final MediaEntry media;
@@ -14,17 +15,51 @@ class MediaEntryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isVideo = media.category.toLowerCase() == 'video';
+
+    String imageUrl;
+
+    if(isVideo){
+      final videoId = YoutubePlayer.convertUrlToId(media.thumbnail);
+      imageUrl = videoId != null ? 'https://img.youtube.com/vi/$videoId/hqdefault.jpg' : " ";
+    } else{
+      imageUrl = 'http://localhost:8000/media-gallery/proxy-image/?url=${Uri.encodeComponent(media.thumbnail)}';
+    }
+
     return GestureDetector(
       onTap: onTap,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16),
-        child: Container(
-          color: Colors.grey[200],
-          child: media.thumbnail.isNotEmpty ?  Image.network(
-            'http://localhost:8000/media-gallery/proxy-image/?url=${Uri.encodeComponent(media.thumbnail)}',
-            fit: BoxFit.cover)
-            : const Center(child: Icon(Icons.image, size: 40),
-          ),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: Stack(
+          children: [
+            // thumbnail
+            Positioned.fill(
+              child: imageUrl.isEmpty ? Container(
+                color: Colors.grey[300],
+                child: const Icon(Icons.broken_image, size: 40),
+              ) : Image.network(
+                imageUrl, 
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            if (isVideo)
+              Positioned.fill(
+                child: Container(
+                  color: Colors.black.withOpacity(0.3),
+                  child: const Center(
+                    child: Icon(
+                      Icons.play_circle_fill,
+                      color: Colors.white,
+                      size: 48,
+                    ),
+                  ),
+                ),
+              ),
+          ],
         ),
       ),
     );
